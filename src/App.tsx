5e2236ad3718e9dfe1c32f155f474095c8b8544d
“HaYat3X” <hayatetakeda48@gmail.com>
2025-05-18 21:25:43 +0900

    カラーモード管理プロバイダーのリファクタリング

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 // import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from './contexts/ThemeContext';
+// import { ThemeProvider } from './provider/ThemeContext';
 import Layout from './components/Layout';
 import HomePage from './pages/HomePage';
 import ArticlesPage from './pages/ArticlesPage';
