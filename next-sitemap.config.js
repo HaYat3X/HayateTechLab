a9ffd236b28aecaea37138417c3b656146d944a9
“HaYat3X” <hayatetakeda48@gmail.com>
2025-09-13 23:46:06 +0900

    issue-#39 SEO対策 #39

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    // ← ここを本番URLに
+    siteUrl: 'https://hayate-tech-lab.vercel.app/',
+    // robots.txt も自動生成
+    generateRobotsTxt: true,
+}
